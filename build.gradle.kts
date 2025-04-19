2c39a5d4c5d1921b55c2ba5044b33c6c64bcad4f
Ondřej Milenovský <wanucha@seznam.cz>
2025-04-19 19:16:38 +0200

    Updated versions

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    java
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "cz.wa"
+version = "1.0-SNAPSHOT"
+
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("junit:junit:4.13.2")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions.jvmTarget.set(JvmTarget.JVM_1_8)
+}
+
+tasks.register<Jar>("fatJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    manifest {
+        attributes(
+            "Implementation-Title" to "Gradle Jar File Example",
+            "Implementation-Version" to version,
+            "Main-Class" to "cz.wa.smoothmetalmap.SmoothMetalMapMain"
+        )
+    }
+
+    from({
+        configurations.runtimeClasspath.get().map {
+            if (it.isDirectory) it else zipTree(it)
+        }
+    })
+    with(tasks.jar.get())
+}
